3b36b31521dd60a497273cf92cfdce30abe439f6
V.Sheynin <sheynin_vlad@rambler.ru>
2025-03-28 21:06:17 +0300

    stream11 - ready

--- src/main/java/Stream11.java
+++ src/main/java/Stream11.java
@@ -3,8 +3,19 @@
 //    - Пропустить `null`-значения.
 //    - Преобразовать строки в числа (`Integer.parseInt`), игнорируя нечисловые строки.
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Stream11 {
     public static void main(String[] args) {
-
+        List<String> listForStream = new ArrayList<>(Arrays.asList("1", "2", "nhb", null, "3"));
+        Stream<String> stream = listForStream.stream();
+        List<Integer> intList = new ArrayList<>();
+        intList = stream.filter((a) -> (!(a == null)&&(a.matches("\\d+"))))
+                .map(i -> Integer.parseInt(i))
+                .toList();
+        System.out.println(intList);
     }
 }
